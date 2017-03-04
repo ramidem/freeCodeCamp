2519b08a7687efc262e15cc51c986fe233bc2af6
Med Aduh <im.ramidem@gmail.com>
2017-03-04 17:01:12 +0800

    update palindrome algorithm

--- basic_algorithm_scripting/palindrome.js
+++ basic_algorithm_scripting/palindrome.js
@@ -4,29 +4,28 @@ remove all non-alphanumeric characters
 turn everything lowercase
 
 */
-function palindrome(str) {
-  // Good luck!
-  var regex = /[\W_]/g;
-  var lowercase = str.toLowerCase().replace(regex, '');
-  var reverse = lowercase.split('').reverse().join('');
 
-  return reverse === lowercase;
+function palindrome(str) {
+	var regex = /[\W_]/g;
+	var lowerReplace = str.toLowerCase().replace(regex, '');
+	var reverse = lowerReplace.split('').reverse().join('');
+
+	return reverse === lowerReplace;
 }
 
 // function palindrome(str) {
-// 	var regex = /[\W_]/g;
-// 	lowercase = str.toLowerCase().replace(regex, '');
-
-// 	var strLength = lowercase.length;
-
-// 	for (var i = 0; i < strLength/2; i++) {
-// 		if (str[1] !== str[strLength - 1 - i]) {
-// 			return true;
-// 		}
-// 	}
-// 	return false;
+//   var regex = /[^A-Za-z0-9]/g;
+//   var lowerReplace = str.toLowerCase().replace(regex, '');
+//
+//   var strLength = lowerReplace.length;
+//
+//   for(var i = 0; i < strLength/2; i++ ) {
+//     if (lowerReplace[i] !== lowerReplace[strLength - 1 - i]) {
+//       return false;
+//     }
+//   }
+//
+//   return true;
 // }
-
-
 
 palindrome("not a palindrome");
