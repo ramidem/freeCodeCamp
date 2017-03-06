6282bacc892c06c203ef3ac093e88e8c4ee81159
Med Aduh <im.ramidem@gmail.com>
2017-03-06 23:33:09 +0800

    Update Confirm Ending

--- basic_algorithm_scripting/confirmEnding.js
+++ basic_algorithm_scripting/confirmEnding.js
@@ -20,9 +20,28 @@ function confirmEnding(str, target) {
 		return false;
 	}
 
+
+// OR just
+
+return str.endsWith(target);
+
 // 	console.log(ending);
 // 	console.log(target);
 }
+
+// https://medium.freecodecamp.com/two-ways-to-confirm-the-ending-of-a-string-in-javascript-62b4677034ac#.yp97l6s1u
+
+function confirmEnding(str, target) {
+  if(str.substr(-target.length) === target) {
+    return true;
+  } else {
+    return false;
+  }
+
+  // alternatively
+  //return  (str.substr(-target.length) === target) ? true : false;
+}
+
 
 console.log(confirmEnding("Bastian", "n"));
 console.log(confirmEnding("He has to give me a new name", "name"));
