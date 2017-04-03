16e9dbc31cad322d7f19709a990fb72a875dc5aa
Med Aduh <im.ramidem@gmail.com>
2017-04-03 15:08:38 +0800

    Reusable Code

---
+++ Basic JavaScript/reusable.js
@@ -0,0 +1,16 @@
+// https://www.freecodecamp.com/challenges/write-reusable-javascript-with-functions
+
+// Example
+function ourReusableFunction() {
+  console.log("Heyya, World");
+}
+
+ourReusableFunction();
+
+// Only change code below this line
+
+function reusableFunction() {
+  console.log("Hi World");
+}
+
+reusableFunction();
