fcc461a14ac9b73e9b23e9fc6501a1725a559ad3
Med Aduh <im.ramidem@gmail.com>
2017-04-03 17:28:57 +0800

    Strict Equality

--- Basic JavaScript/comparison.js
+++ Basic JavaScript/comparison.js
@@ -1,6 +1,6 @@
 // https://www.freecodecamp.com/challenges/comparison-with-the-equality-operator
 
-// Setup
+// LOOSE EQUALITY
 function testEqual(val) {
   if (val == 12) { // Change this line
     return "Equal";
@@ -10,3 +10,17 @@ function testEqual(val) {
 
 // Change this value to test
 testEqual(10);
+
+
+// STRICT EQUALITY
+
+// Setup
+function testStrict(val) {
+  if (val === 7) { // Change this line
+    return "Equal";
+  }
+  return "Not Equal";
+}
+
+// Change this value to test
+testStrict(10);
