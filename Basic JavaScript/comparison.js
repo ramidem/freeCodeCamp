9a9ea9e3da8735fa19a97a48f2f50dfb7fc65e12
Med Aduh <im.ramidem@gmail.com>
2017-04-03 17:27:08 +0800

    Comparison

---
+++ Basic JavaScript/comparison.js
@@ -0,0 +1,12 @@
+// https://www.freecodecamp.com/challenges/comparison-with-the-equality-operator
+
+// Setup
+function testEqual(val) {
+  if (val == 12) { // Change this line
+    return "Equal";
+  }
+  return "Not Equal";
+}
+
+// Change this value to test
+testEqual(10);
