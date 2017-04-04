3ccdece7af40bb60be29dd272bc39751aafed7cb
Med Aduh <im.ramidem@gmail.com>
2017-04-04 23:00:53 +0800

    While Loop

---
+++ Basic JavaScript/whileLoop.js
@@ -0,0 +1,11 @@
+// https://www.freecodecamp.com/challenges/iterate-with-javascript-while-loops
+
+// Setup
+var myArray = [];
+
+// Only change code below this line.
+var i = 0;
+while (i < 5) {
+  myArray.push(i);
+  i++;
+}
