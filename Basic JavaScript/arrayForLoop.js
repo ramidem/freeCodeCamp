393c3a915edd73652fef5bc1bb141b62c83f85b2
Med Aduh <im.ramidem@gmail.com>
2017-04-04 22:49:57 +0800

    Iterate an Array with For Loop

---
+++ Basic JavaScript/arrayForLoop.js
@@ -0,0 +1,19 @@
+// https://www.freecodecamp.com/challenges/iterate-through-an-array-with-a-for-loop
+
+// Example
+var ourArr = [ 9, 10, 11, 12];
+var ourTotal = 0;
+
+for (var i = 0; i < ourArr.length; i++) {
+  ourTotal += ourArr[i];
+}
+
+// Setup
+var myArr = [ 2, 3, 4, 5, 6];
+
+var total = 0;
+for (var j = 0; j < myArr.length; j++) {
+
+  total += myArr[j];
+}
+// Only change code below this line
