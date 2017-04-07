296e1b6fe6da5a09f1aadfd4d1481edd721a34aa
Med Aduh <im.ramidem@gmail.com>
2017-04-07 21:32:50 +0800

    Added methods to reverse string

--- Basic Algorithm Scripting/reverseString.js
+++ Basic Algorithm Scripting/reverseString.js
@@ -1,7 +1,18 @@
 function reverseString(str) {
+
+  // All them methods
   var split = str.split('');
   var reverse = split.reverse();
   var join = reverse.join('');
+
+  // All in one
+  // return str.split('').reverse().join('');
+
+  // For Loop
+  var join = "";
+  for (var i = str.length - 1; i >= 0; i--) {
+    join += str[i];
+  }
 
   return join;
 }
