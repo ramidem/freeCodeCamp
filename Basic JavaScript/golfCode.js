df8133344235efe8ec5d41f2f0a8206bd3b936cf
Med Aduh <im.ramidem@gmail.com>
2018-07-11 17:00:22 +0800

    Golf Code

--- Basic JavaScript/golfCode.js
+++ Basic JavaScript/golfCode.js
@@ -1,26 +1,26 @@
-// https://www.freecodecamp.com/challenges/golf-code
+var names = ["Hole-in-one!", "Eagle", "Birdie", "Par", "Bogey", "Double Bogey", "Go Home!"];
 
 function golfScore(par, strokes) {
   // Only change code below this line
-
-  if (strokes === 1) {
-    return "Hole-in-one!";
-  } else if (strokes <= par -2) {
-    return "Eagle";
-  } else if (strokes === par - 1) {
-    return "Birdie";
-  } else if (strokes === par) {
-    return "Par";
-  } else if (strokes === par + 1) {
-    return "Bogey";
-  } else if (strokes === par + 2) {
-    return "Double Bogey";
+  if (strokes == 1) {
+    name = names[0];
+  } else if (strokes <= par - 2) {
+    name = names[1];
+  } else if (strokes == par - 1) {
+    name = names[2];
+  } else if (strokes == par) {
+    name = names[3];
+  } else if (strokes == par + 1) {
+    name = names[4];
+  } else if (strokes == par + 2) {
+    name = names[5];
   } else if (strokes >= par + 3) {
-    return "Go Home!";
+    name = names[6];
   }
 
+  return name;
   // Only change code above this line
 }
 
 // Change these values to test
-golfScore(1);
+console.log(golfScore(5, 4))
