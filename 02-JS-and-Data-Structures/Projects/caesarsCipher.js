4a78ae5c47a795ef4ed644137e42c655811313f5
Med Aduh <im.ramidem@gmail.com>
2019-12-20 07:48:13 +0800

    projects: caecar's cipher

---
+++ 02-JS-and-Data-Structures/Projects/caesarsCipher.js
@@ -0,0 +1,30 @@
+function rot13(str) {
+  // LBH QVQ VG!
+  let letters = str.toUpperCase();
+
+  // console.log(String.fromCharCode("".toUpperCase().charCodeAt() + 13));
+  console.log("M".toUpperCase().charCodeAt());
+  let match = [];
+
+  for (let i = 0; i < letters.length; i++) {
+    if (letters[i].match(/[A-Z]/)) {
+      // if (letters[i].charCodeAt() < 78) {
+      //   match.push(String.fromCharCode(letters[i].charCodeAt() + 13));
+      // } else {
+      //   match.push(String.fromCharCode(letters[i].charCodeAt() - 13));
+      // }
+      match.push(
+        String.fromCharCode(
+          `${letters[i].charCodeAt()} ${
+            letters[i].charCodeAt() < 78 ? "+" : "-"
+          } ${13}`
+        )
+      );
+    } else {
+      match.push(letters[i]);
+    }
+  }
+  return match.join("");
+}
+
+console.log(rot13("SERR PBQR PNZC"));
